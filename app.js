7f6676ace67d64d6e5458725cf5af0d052e6f2cc
Joaquín Franco <joacofranco27@hotmail.com>
2025-04-16 11:58:08 -0300

    primer js

---
+++ app.js
@@ -0,0 +1,11 @@
+let nombreProducto = "Celular"
+let precioUnitario = 500000
+let unidades = prompt("¿Cuantas unidades de " + nombreProducto " desea comprar?")
+unidades = Number(unidades)
+let precioFinal = precioUnitario * unidades
+
+alert(`Resumen final de la compra:
+    Producto: ${nomnbreProducto} 
+    Precio Unitario: $${precioUnitario} 
+    Unidades: ${unidades} 
+    Precio final: $${precioFinal}`);
